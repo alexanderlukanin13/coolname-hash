8c0b022884c12580efaf46855f5e2e01372e8841
Alexander Lukanin <alexander.lukanin.13@gmail.com>
2026-03-17 00:59:16 +0600

    version 1.0

---
+++ src/coolname_hash/__init__.py
@@ -0,0 +1,98 @@
+import struct
+from random import Random
+
+import coolname
+from coolname.impl import _md5
+
+from ._version import __version__, __version_tuple__  # noqa
+
+
+__all__ = ['RandomGenerator', 'generate', 'generate_slug', 'get_combinations_count', 'pseudohash', 'pseudohash_slug']
+
+
+_bytes_to_int = struct.Struct('<q').unpack
+
+
+def _poor_mans_rng(seed: int, length: int):
+    # Dumb-but-cheap replacement for RNG to retry after bad combinations
+    # (repeated prefixes, too long, etc.)
+    max_pow = len(str(abs(length)))
+    cur_pow = 0
+    i = seed
+    while True:
+        i = i + 10**cur_pow
+        yield i % length
+        cur_pow = (cur_pow + 1) % max_pow
+
+
+class RandomGenerator(coolname.RandomGenerator):
+    """Drop-in replacement for coolname.RandomGenerator"""
+
+    def pseudohash(self, obj: int | str | bytes, pattern: str | int | None = None) -> list[str]:
+        """
+        Similar to generate(), but returns deterministic result for a hashable object.
+
+        WARNING: collisions are possible - uniqueness not guaranteed!
+        """
+        lst = self._lists[pattern]
+        # NOTE: hash will be the same for 123, '123' and b'123' - this is intentional
+        if isinstance(obj, str):
+            obj = obj.encode('utf8')
+        elif isinstance(obj, int) and not isinstance(obj, bool):
+            obj = str(obj).encode('utf8')
+        elif not isinstance(obj, bytes):
+            raise TypeError(f'Unexpected type "{type(obj).__qualname__}" (must be int, str or bytes): {obj!r}')
+        i = int.from_bytes(_md5(obj).digest()[:8]) % lst.length  # type: ignore
+        while True:
+            result = lst[i]
+            # 1. Check that there are no duplicates
+            # 2. Check that there are no duplicate prefixes
+            # 3. Check max slug length
+            n = len(result)
+            if (self._ensure_unique and len(set(result)) != n or
+                    self._check_prefix and len(set(x[:self._check_prefix] for x in result)) != n or
+                    self._max_slug_length and sum(len(x) for x in result) + n - 1 > self._max_slug_length):
+                try:
+                    i = next(p)  # type: ignore
+                except NameError:
+                    p = _poor_mans_rng(i, lst.length)
+                    i = next(p)
+                continue
+            # Most of the time it returns at first attempt, without repeating the loop.
+            # Note about typing: technically its List[str] | str, but we know it's always List[str] at this point.
+            return result  # type: ignore
+
+    def pseudohash_slug(self, obj: int | str | bytes, pattern: str | int | None = None) -> str:
+        """
+        Similar to generate_slug(), but returns deterministic result for a hashable object.
+
+        WARNING: collisions are possible - uniqueness not guaranteed!
+        """
+        return '-'.join(self.pseudohash(obj, pattern))
+
+
+class _DefaultRandomGenerator(RandomGenerator):
+
+    def __init__(self, default: coolname.RandomGenerator):  # noqa
+        self.random = default.random
+        self._lists = default._lists
+        self._ensure_unique = default._ensure_unique
+        self._check_prefix = default._check_prefix
+        self._max_slug_length = default._max_slug_length
+
+
+# Default generator is a global object
+_default: RandomGenerator = _DefaultRandomGenerator(coolname.impl._default)
+
+# Global functions are actually methods of the default generator.
+# (most users don't care about creating generator instances)
+generate = _default.generate
+generate_slug = _default.generate_slug
+get_combinations_count = _default.get_combinations_count
+pseudohash = _default.pseudohash
+pseudohash_slug = _default.pseudohash_slug
+
+
+def replace_random(rand: Random | None = None) -> None:
+    """Replaces random number generator for the default RandomGenerator instance."""
+    _default.random = rand
---
+++ tests/test_coolname_hash.py
@@ -0,0 +1,49 @@
+import re
+from random import Random
+
+import pytest
+
+from coolname_hash import pseudohash_slug, _md5  # noqa
+
+from src.coolname_hash import replace_random, generate_slug
+
+
+def test_md5():
+    """Ensure consistency of MD5 between Python releases. Just in case."""
+    assert _md5(b'abc').digest() == b'\x90\x01P\x98<\xd2O\xb0\xd6\x96?}(\xe1\x7fr'
+
+def test_pseudohash_slug():
+    assert pseudohash_slug('abc') == 'hopeful-ultra-doberman-of-leadership'
+
+
+def test_int_str_bytes_same():
+    assert pseudohash_slug('123456') == pseudohash_slug(b'123456')
+    assert pseudohash_slug('123456') == pseudohash_slug(123456)
+    assert pseudohash_slug('-123456') == pseudohash_slug(b'-123456')
+    assert pseudohash_slug('-123456') == pseudohash_slug(-123456)
+    assert pseudohash_slug('') == pseudohash_slug(b'')
+
+
+def test_unsupported_types():
+    with pytest.raises(TypeError, match=re.escape('Unexpected type "NoneType" (must be int, str or bytes): None')):
+        pseudohash_slug(None)
+    with pytest.raises(TypeError, match=re.escape('Unexpected type "bool" (must be int, str or bytes): True')):
+        pseudohash_slug(True)
+    with pytest.raises(TypeError, match=re.escape('Unexpected type "tuple" (must be int, str or bytes): ()')):
+        pseudohash_slug(tuple())
+    with pytest.raises(TypeError, match=re.escape('Unexpected type "list" (must be int, str or bytes): []')):
+        pseudohash_slug(list())
+
+
+def test_coverage():
+    """Cover retries - at least one must happen"""
+    slugs = set(pseudohash_slug(i) for i in range(1000))
+    assert len(slugs) == 1000  # all must be unique, statistically
+
+
+def test_coverage_replace_random():
+    """There is a separate implementation of replace_random, so we need to cover it."""
+    rand = Random()
+    replace_random(rand)
+    rand.seed(0)
+    assert generate_slug() == 'lyrical-benevolent-swine-of-expertise'
